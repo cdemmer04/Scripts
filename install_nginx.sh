eb5eb515f12568bf8e93272c6f1794dfd13a71b1
Chiel Demmer <chiel.demmer@gmail.com>
2025-04-15 10:44:44 +0000

    added nginx

---
+++ install_nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Chiel Demmer
+
+# Install prerequisites
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+
+# Add Nginx repository
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | sudo tee /etc/apt/preferences.d/99nginx
+
+# Install NGINX
+sudo apt update
+sudo apt install nginx
