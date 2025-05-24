1c6b7ef578af8692377db805134a2328a85ade19
Chiel Demmer <117164532+cdemmer04@users.noreply.github.com>
2025-05-24 09:56:24 +0200

    Update install_adds.ps1

--- install_adds.ps1
+++ install_adds.ps1
@@ -1,9 +1,14 @@
+param(
+    [String]
+    $domainName = "domain.local"
+)
+
 # Set variables
 $passwd = ConvertTo-SecureString -AsPlainText "Pa55w.rd" -Force
-$domainName = "CDEMMER546163.LOCAL"
 
+Write-Host $domainName
 # Install management tools for adds
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+# Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
-# Set up adds domain
-Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $passwd -InstallDns -Force
+# # Set up adds domain
+# Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $passwd -InstallDns -Force
