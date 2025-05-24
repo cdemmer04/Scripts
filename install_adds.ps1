bf874414400dc983baf0bec620e725489455d370
Chiel Demmer <117164532+cdemmer04@users.noreply.github.com>
2025-05-24 12:07:37 +0200

    Update install_adds.ps1

--- install_adds.ps1
+++ install_adds.ps1
@@ -8,7 +8,7 @@ $passwd = ConvertTo-SecureString -AsPlainText "Pa55w.rd" -Force
 
 Write-Host $domainName
 # Install management tools for adds
-# Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
 # # Set up adds domain
-# Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $passwd -InstallDns -Force
+Install-ADDSForest -DomainName $domainName -SafeModeAdministratorPassword $passwd -InstallDns -Force
