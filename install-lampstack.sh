19310c20ae771025a32597d41c24b9210c23a53f
cdemmer04 <chiel.demmer@gmail.com>
2025-10-26 08:38:45 +0000

    opzet

---
+++ install-lampstack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define variables
+phpVersion=8.3
+scriptUrl="https://r.chieldemmer.nl"
+nginxInstallScript="install-nginx.sh"
+mariadbInstallScript="install-mariadb.sh"
+
+# Install nginx
+#TODO: Fix wget $nginxInstallScript
+apt update
+apt install nginx -y
+
+# Install php-fpm for nginx
+apt install "php$phpVersion-fpm" -y
+
+# Install mariadb
+wget "$scriptUrl/$mariadbInstallScript" 
